d6d8b92ea12f5c8e2416c7ae64f83909598f31b7
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-12-12 22:09:03 +0900

    feat: 월 enum class 생성

--- src/main/kotlin/oncall/model/Month.kt
+++ src/main/kotlin/oncall/model/Month.kt
@@ -2,6 +2,19 @@ package oncall.model
 
 enum class Month(
     val month: Int,
+    val days: Int,
     val holiday: List<Int>?
 ) {
+    JANUARY(1, 31, listOf(1)),
+    FEBRUARY(2,28, null),
+    MARCH(3, 31, listOf(1)),
+    APRIL(4, 30, null),
+    MAY(5, 31, listOf(5)),
+    JUNE(6, 30, listOf(6)),
+    JULY(7, 31, null),
+    AUGUST(8, 31, listOf(15)),
+    SEPTEMBER(9, 30, null),
+    OCTOBER(10, 31, listOf(3, 9)),
+    NOVEMBER(11, 30, null),
+    DECEMBER(12, 31, listOf(25));
 }
